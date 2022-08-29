a01f4d50260674f78dac76b40c09de7338950e33
Eron Alves <eron.a.silva@capgemini.com>
2022-08-29 09:31:25 -0300

    [Footer] implemented

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -20,4 +20,30 @@ describe('NavbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a <nav> element inside', ()=>{
+    const el: HTMLElement = fixture.nativeElement;
+    const navEl: HTMLElement = <HTMLElement> el.querySelector('nav');
+
+    expect(navEl).toBeTruthy();
+  });
+
+  it('nav element should be d-flex, container-fluid, and have blue color as mockup', ()=>{
+    const el: HTMLElement = fixture.nativeElement;
+    const navEl: HTMLElement = <HTMLElement> el.querySelector('nav');
+    const classes: string = navEl.classList.value;
+    const styleBgColor: string = getComputedStyle(navEl).backgroundColor;
+
+    expect(classes).toContain('d-flex');
+    expect(classes).toContain('container-fluid');
+    expect(styleBgColor).toBe('rgb(8, 61, 119)');
+  });
+
+  it('nav element have to be size defined in mockup', ()=>{
+    const el: HTMLElement = fixture.nativeElement;
+    const navEl: HTMLElement = <HTMLElement> el.querySelector('nav');
+    const styleHeight: string = getComputedStyle(navEl).height;
+
+    expect(styleHeight).toBe('48px');
+  })
 });
