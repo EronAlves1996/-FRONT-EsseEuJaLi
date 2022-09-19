2031c7494871bdacbbf6a5f61794c7de6edd59fc
Eron Alves <eron.a.silva@capgemini.com>
2022-09-19 17:09:16 -0300

    [CATLOG] Implemented pagination of search

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -10,22 +10,38 @@ import { Book } from '../shared/book';
 export class CatalogComponent implements OnInit {
 
   books: Book[] = [];
-  searchBook:string = ''
+  searchBook:string = 'Harry Potter';
+  totalPages: number[] = [];
+  startIndex:number = 0;
+  actualPage: number = 1;
 
   constructor(
     private bookService:BookService
   ) { }
 
   ngOnInit(): void {
-    this.bookService.search("random").subscribe(searchResult => {
-      this.books = searchResult.items;
-    })
+    this.search();
   }
 
   search(): void{
     this.bookService.search(encodeURI(this.searchBook.replace(' ', '+').toLocaleLowerCase())).subscribe(searchResult=> {
+      this.totalPages = []
+      for(let i = 0; i < Math.floor((searchResult.totalItems)/20); i++){
+        this.totalPages.push(i+1);
+      };
       this.books = searchResult.items.filter(n => n.volumeInfo.imageLinks);
     });
+  }
+
+  searchOnEnter(e: any): void {
+    if(e.key === "Enter") this.search();
+  }
+
+  goToIndexInSearch(index: number): void {
+    this.bookService.search(this.searchBook, index * 20).subscribe(searchResult=> {
+      this.books = searchResult.items.filter(n=>n.volumeInfo.imageLinks);
+      this.actualPage = index + 1;
+    })
   }
 
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -11,7 +11,7 @@ export class BookService {
   private url: string = "https://www.googleapis.com/books/v1/volumes?key=AIzaSyB13cqdwanOGh_UcPJR04luUaT-iEXcRrY&";
   constructor(private http: HttpClient) { }
 
-  search(searchParameter:string): Observable<Search> {
-    return this.http.get<Search>(this.url + "q=" + searchParameter + "&maxResults=20");
+  search(searchParameter:string, startIndex:number = 0): Observable<Search> {
+    return this.http.get<Search>(this.url + encodeURI("q=" + searchParameter + "&maxResults=20&startIndex=" + startIndex));
   }
 }
